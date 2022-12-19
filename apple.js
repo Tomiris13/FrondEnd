788ee28745603f7114d7560a8f84a537e480701a
Tomiris Orynbaeva <tomi07200313@gmail.com>
2022-12-20 01:02:44 +0600

    i commit

---
+++ apple.js
@@ -0,0 +1,17 @@
+function changePhoto(photo){
+    document.querySelector('.phone').src = photo;
+}
+function changeColor(color){
+    const detail1=document.querySelector('.detail1');
+    detail1.style.background=color;
+}
+
+function changePhoto(photo){
+    document.querySelector('.ipad').src = photo;
+}
+function changeColor(color){
+    const detail2=document.querySelector('.detail2');
+    detail2.style.background=color;
+}
+
+
